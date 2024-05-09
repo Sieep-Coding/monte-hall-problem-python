84512bd314f5dbd1b0637132370d555dd9a15b9d
Sieep-Coding <nstamb6@gmail.com>
2024-05-09 07:11:31 -0700

    first visual

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import plotly.graph_objs as go
 
+data = [go.Bar(x=['Switch', 'Stay'], y=[2/3, 1/3])]
+
+layout = go.Layout(title='Monty Hall Problem',
+                   xaxis=dict(title='Strategy'),
+                   yaxis=dict(title='Probability of Winning'),
+                   showlegend=False)
+
+fig = go.Figure(data=data, layout=layout)
+fig.show()
