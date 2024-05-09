b5f288156b2465bd3a7f09f261ab21d873c4b1fd
Sieep-Coding <nstamb6@gmail.com>
2024-05-09 07:17:50 -0700

    second visual

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import plotly.graph_objs as go
 
-data = [go.Bar(x=['Switch', 'Stay'], y=[2/3, 1/3])]
 
+## Visualize the probabability of the Monty Hall Problem
+## X represents the strategy taken.
+## Y Represents the probability of each strategy.
+data = [go.Bar(x=['Switch', 'Stay'], y=[2/3, 1/3])]
 layout = go.Layout(title='Monty Hall Problem',
                    xaxis=dict(title='Strategy'),
                    yaxis=dict(title='Probability of Winning'),
                    showlegend=False)
-
 fig = go.Figure(data=data, layout=layout)
-fig.show()
+## fig.show()  <-- Uncomment here. ##
+
+
+## Visualize using a pie chart instead.
+pie_data = [go.Pie(labels=['Win', 'Lose'], values=[2, 1])]
+pie_layout = go.Layout(title='Monty Hall Problem',
+                       showlegend=False)
+pie_fig = go.Figure(data=pie_data, layout=pie_layout)
+pie_fig.show()
+
